3f9eb57825c61780690c0fe49f3782966e80e3f8
student <student@aprotrain-aptech.edu>
2018-09-08 20:48:30 +0700

    hahah

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('student')->insert([
+        	'username'=>"Tran Van Diep",
+        	'email'=>"a@gmail.com"
+        ]);
+    }
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Laravel Tutorial</title>
+</head>
+<body>
+	<h1 style="text-align: center;">Xin chao</h1>
+</body>
+</html>
